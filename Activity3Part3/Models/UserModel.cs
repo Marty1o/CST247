eaf73eac2b18567af9c44a3efcd1c8b90a0f66a5
Marty1o <52226629+Marty1o@users.noreply.github.com>
2020-11-22 16:22:50 -0700

    Act 2 part 3

---
+++ Activity3Part3/Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Activity1Part3.Models
+{
+    public class UserModel
+    {
+
+        [Required]
+        [Display(Name = "Username")]
+        [StringLength(20, MinimumLength = 4)]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [StringLength(20, MinimumLength = 4)]
+        public string Password { get; set; }
+
+    }
+}
---
+++ Activity3Part3/Services/Data/SecurityDAO.cs
@@ -0,0 +1,35 @@
+﻿using Activity1Part3.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Activity1Part3.Services.Data
+{
+    public class SecurityDAO
+    {
+        public bool FindByUser(UserModel user)
+        {
+
+           // String connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            SqlCommand cmnd = new SqlCommand("SELECT * FROM Users WHERE Username = '" + user.Username + "' AND Password = '" + user.Password + "'", conn);
+
+            conn.Open();
+            SqlDataReader reader = cmnd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
